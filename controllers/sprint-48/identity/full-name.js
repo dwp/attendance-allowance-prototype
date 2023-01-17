57bacb6bb3cca03a6e94d1fbf37f194fdd2a45e6
Craig Taylor <craig.taylor3@engineering.digital.dwp.gov.uk>
2023-01-17 13:56:59 +0000

    Merge branch 'feat/new-full-name-page' into 'main'
    Feat/new full name page
    
    See merge request dwp/apply-for-attendance-allowance/prototype-attendance-allowance!3
    
    GitOrigin-RevId: 1978636b684928b3b9556fe6a7f9ae877e596664

--- controllers/sprint-48/identity/full-name.js
+++ controllers/sprint-48/identity/full-name.js
@@ -9,17 +9,32 @@ const config = {
   name: urls.fullName,
   previous: urls.nationalInsurance,
   next: urls.homeAddressPostcode,
-  validation: {
-    type: validation.textInput,
-    options: {
-      minLength: 1,
-      maxLength: 500,
+  validation: [
+    {
+      name: 'first-name',
+      type: validation.textInput,
+      options: {
+        minLength: 1,
+        maxLength: 50,
+      },
+      errors: {
+        required: 'Enter your first name.',
+        maxLength: 'First name must be 50 characters or fewer.',
+      },
     },
-    errors: {
-      required: 'Enter your full name.',
-      maxLength: 'Full name must be 500 characters or fewer.',
+    {
+      name: 'last-name',
+      type: validation.textInput,
+      options: {
+        minLength: 1,
+        maxLength: 100,
+      },
+      errors: {
+        required: 'Enter your last name.',
+        maxLength: 'Last name must be 100 characters or fewer.',
+      },
     },
-  },
+  ],
 };
 
 module.exports = registerController(config.name, config);
