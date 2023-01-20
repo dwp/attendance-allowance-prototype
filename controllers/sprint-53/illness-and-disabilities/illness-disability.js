6b56e9bdffc829b048af380a09033e8561197288
Alicia Perez Gomez <alicia.perezgomez@engineering.digital.dwp.gov.uk>
2023-01-20 13:15:42 +0000

    checked routing is correct

--- controllers/sprint-53/illness-and-disabilities/illness-disability.js
+++ controllers/sprint-53/illness-and-disabilities/illness-disability.js
@@ -1,13 +1,31 @@
 const {
   urls,
   validation,
+  match,
   registerController,
 } = require('../../../utils/controller');
 
 const config = {
   name: urls.illnessDisability,
   next: urls.illnessDisabilityAdded,
-  previous: urls.careHomeHospital,
+  previous: [
+    {
+      page: urls.careHomeHospital,
+      condition: {
+        field: urls.careHomeHospital, 
+        value: 'yes',
+        match: match.value, 
+      }, 
+    },
+    {
+      page: urls.sercoCareHomeHospital,
+      condition: {
+        field: urls.careHomeHospital, 
+        value: 'no',
+        match: match.value, 
+      }, 
+    },
+  ],
   validation: {
     type: validation.textInput,
     errors: {
