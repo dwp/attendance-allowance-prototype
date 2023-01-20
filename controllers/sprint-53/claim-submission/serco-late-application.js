04629eacd71b890981068c9618087fd865fd14f2
Alicia Perez Gomez <alicia.perezgomez@engineering.digital.dwp.gov.uk>
2023-01-20 11:21:15 +0000

    added content and error message

--- controllers/sprint-53/claim-submission/serco-late-application.js
+++ controllers/sprint-53/claim-submission/serco-late-application.js
@@ -1,5 +1,6 @@
 const {
   urls,
+  validation,
   registerController,
 } = require('../../../utils/controller');
 
@@ -7,6 +8,18 @@ const config = {
   name: urls.sercoLateApplication,
   next: urls.claimSubmission,
   previous: urls.checkAnswersContactDetails,
+  validation: {
+    name: 'serco-late-application',
+    type: validation.textInput,
+    options: {
+      minLength: 1,
+      maxLength: 100,
+    },
+    errors: {
+      required: 'Enter a reason why you are submitting your application late.',
+      maxLength: 'Reason must be 100 characters or fewer.',
+    },
+  },
 };
 
 module.exports = registerController(config.name, config);
