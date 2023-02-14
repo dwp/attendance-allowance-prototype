012c386150e5546de20a90f36db59e8adce4541b
Alicia Perez Gomez <alicia.perezgomez@engineering.digital.dwp.gov.uk>
2023-02-14 16:23:19 +0000

    added telephone-number-other

---
+++ controllers/sprint-54/contact-details/telephone-number-other.js
@@ -0,0 +1,28 @@
+const {
+  urls,
+  validation,
+  match,
+  registerController,
+} = require('../../../utils/controller');
+
+const config = {
+  name: urls.telephoneNumberOther,
+  previous: urls.addContactDetails,
+  next: urls.addContactDetails,
+  validation: [
+    {
+      name: 'main-contact-number-other',
+      type: validation.textInput,
+      options: {
+        minLength: 5,
+        maxLength: 15,
+      },
+      errors: {
+        required: 'Your main contact telephone number must be in the correct format, like 01632 960 001, 07700 900 982 or +44 808 157 0192. For international numbers include the country code.',
+        maxLength: 'Telephone number must be 15 characters or less.',
+      },
+    },
+  ],
+};
+
+module.exports = registerController(config.name, config);
