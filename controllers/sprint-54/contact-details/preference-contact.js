97fd6a956d0c487d4049416b695bfd1f1a5b6c5b
Alicia Perez Gomez <alicia.perezgomez@engineering.digital.dwp.gov.uk>
2023-02-14 17:08:05 +0000

    content update in preference-contact and next routing to email-confirm

--- controllers/sprint-54/contact-details/preference-contact.js
+++ controllers/sprint-54/contact-details/preference-contact.js
@@ -8,19 +8,7 @@ const {
 const config = {
   name: urls.preferenceContact,
   previous: urls.telephoneNumberOther,
-  next: [
-    {
-      page: urls.residenceCountryIneligible,
-      condition: {
-        field: urls.residenceCountry,
-        value: 'abroad',
-        match: match.value,
-      },
-    },
-    {
-      page: urls.countryOutsideUKDuration,
-    },
-  ],
+  next: urls.emailConfirm,
   validation: {
     type: validation.radios,
     errors: {
