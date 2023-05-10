7b94636b890695af844f113616b5acf1491a4fee
Nathan Maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2023-05-10 15:01:26 +0000

    Merge branch 'feat/reveal-start-date' into 'main'
    added validation in reveal
    
    See merge request dwp/apply-for-attendance-allowance/prototype-attendance-allowance!66
    
    GitOrigin-RevId: 875f2269fa24564842aa018ec141997459afed29

--- controllers/sprint-60-1/care-needs-day/difficulties-start-date.js
+++ controllers/sprint-60-1/care-needs-day/difficulties-start-date.js
@@ -8,9 +8,19 @@ const config = {
   name: urls.difficultiesStartDate,
   previous: urls.checkAnswersNight,
   next: urls.checkAnswersStartDate,
-  validation: {
-    type: validation.dateInput,
-  },
+  validation: [
+    {
+      name: 'start-date',
+      type: validation.radios,
+    },
+    {
+      name: 'difficulties-start-date',
+      type: validation.dateInput,
+      condition: {
+        field: 'start-date',
+        value: 'less-than-six-months', 
+      },
+    }]
 };
 
 module.exports = registerController(config.name, config);
