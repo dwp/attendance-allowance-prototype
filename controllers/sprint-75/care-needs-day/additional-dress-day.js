5858d95e1937b499b2d5cd4b14e693f242b4a455
James Gordon <james.gordon@engineering.digital.dwp.gov.uk>
2023-11-27 16:17:25 +0000

    feat(aag-3192): initial commit

---
+++ controllers/sprint-75/care-needs-day/additional-dress-day.js
@@ -0,0 +1,79 @@
+const {
+  urls,
+  validation,
+  match,
+  registerController,
+} = require('../../../utils/controller');
+
+const config = {
+  name: urls.additionalDressDay,
+  previous: urls.dressDayWhy,
+  next: [
+    {
+      page: urls.movingIndoorsDay,
+      condition: {
+        field: urls.dayDifficulties,
+        value: ['daytime-moving'],
+        match: match.anyOne,
+      },
+    },
+    {
+      page: urls.fallingOrStumbling,
+      condition: {
+        field: urls.dayDifficulties,
+        value: ['stumbling-falling'],
+        match: match.anyOne,
+      },
+    },
+    {
+      page: urls.toiletDay,
+      condition: {
+        field: urls.dayDifficulties,
+        value: ['daytime-toilet'],
+        match: match.anyOne,
+      },
+    },
+    {
+      page: urls.eatDrinkDay,
+      condition: {
+        field: urls.dayDifficulties,
+        value: ['daytime-eating'],
+        match: match.anyOne,
+      },
+    },
+    {
+      page: urls.medicationTreatmentDay,
+      condition: {
+        field: urls.dayDifficulties,
+        value: ['daytime-medication'],
+        match: match.anyOne,
+      },
+    },
+    {
+      page: urls.socialActivitiesDay,
+      condition: {
+        field: urls.dayDifficulties,
+        value: ['daytime-hobbies'],
+        match: match.anyOne,
+      },
+    },
+    {
+      page: urls.communicationDay,
+      condition: {
+        field: urls.dayDifficulties,
+        value: ['daytime-communication'],
+        match: match.anyOne,
+      },
+    },
+    {
+      page: urls.keepEyeIntroduction,
+    },
+  ],
+  validation: [
+    {
+      name: 'additional-dress-day',
+      type: validation.radios,
+    },
+  ],
+};
+module.exports = registerController(config.name, config);
