7ad2933e96b2111bc6b9662a74993fc7cc77181b
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2023-12-05 14:56:29 +0000

    Feat/cya details fixes
    GitOrigin-RevId: a9c691e8a9982a003414fd4a85ec1fd787b8046c

--- controllers/sprint-75/consent/consent.js
+++ controllers/sprint-75/consent/consent.js
@@ -1,17 +1,46 @@
 const {
     urls,
     validation,
+    match,
     registerController,
   } = require('../../../utils/controller');
   
+  
   const config = {
     name: urls.consent,
-    previous: urls.treatmentSurgery,
-    next: urls.surgeryMedicalCentre,
+    previous: [
+      {
+        page: urls.treatmentSurgeryInformation,
+        condition: {
+          field: urls.treatmentSurgery,
+          value: 'yes',
+          match: match.value,
+        },
+      },
+      {
+        page: urls.treatmentSurgery,
+      },
+    ],
+    next: [
+      {
+        page: urls.surgeryMedicalCentre,
+        condition: {
+          field: urls.consent,
+          value: 'yes',
+          match: match.value,
+        },
+      },
+      {
+        page: urls.evidence,
+      },
+    ],
     validation: {
       type: validation.radios,
+      errors: {
+        required: 'Select yes, if you agree that information about your health can be shared with DWP or the healthcare professionals that work for them.',
+      },
     },
   };
-  
+
   module.exports = registerController(config.name, config);
   
--- controllers/sprint-75/surgery-or-treatment/treatment-surgery-information.js
+++ controllers/sprint-75/surgery-or-treatment/treatment-surgery-information.js
@@ -18,7 +18,7 @@ const config = {
       },
     },
     {
-      page: urls.checkAnswersDetails,
+      page: urls.consent,
     },
   ],
 
