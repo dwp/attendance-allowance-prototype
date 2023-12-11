b7fbdace8f5dfa129441c00cedda399da5679ff3
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2023-12-11 10:17:05 +0000

    refactor: content changes and error messages
    GitOrigin-RevId: 86feba2f3d6379fc07607dc1f52e880a9883ff95

--- controllers/sprint-75/eligibility/who-is-applying-ineligible.js
+++ controllers/sprint-75/eligibility/who-is-applying-ineligible.js
@@ -7,53 +7,16 @@ const {
   const config = {
     name: urls.whoIsApplyingIneligible,
     previous: [
-     {
-        page: urls.bestDescribe,
-        condition: {
-            field: urls.bestDescribe,
-            value: 'powerOfAttorney',
-            match: match.value,
-        },
-      },
       {
         page: urls.bestDescribe,
         condition: {
-            field: urls.bestDescribe,
-            value: 'appointed',
-            match: match.value,
-        },
-      },
-      {
-        page: urls.bestDescribe,
-        condition: {
-            field: urls.bestDescribe,
-            value: 'appointee',
-            match: match.value,
-        },
-      },
-      {
-        page: urls.bestDescribe,
-        condition: {
-            field: urls.bestDescribe,
-            value: 'cab',
-            match: match.value,
-        },
-      },
-      {
-        page: urls.bestDescribe,
-        condition: {
-            field: urls.bestDescribe,
-            value: 'curator',
-            match: match.value,
-        },
-      },
-      {
-        page: urls.specialRulesDeclaration,
-        condition: {
-          field: urls.specialRulesDeclaration,
-          value: 'yes',
+          field: urls.whoIsApplying,
+          value: 'someone-else',
           match: match.value,
         },
+      },
+      {
+        page: urls.whoIsApplying,
       },
     ],
   };
