c039e2e3203e31c0cb99d33e10b91b8c5ece576d
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2023-12-12 14:07:59 +0000

    feat: change tick boxes to radios
    GitOrigin-RevId: ed06e49a8875d79bfb8a0b9cfd860273eb277c8f

--- controllers/sprint-75/contact-details/alternative-formats.js
+++ controllers/sprint-75/contact-details/alternative-formats.js
@@ -1,16 +1,15 @@
 const {
   urls,
   validation,
-  match,
   registerController,
-} = require("../../../utils/controller");
+} = require('../../../utils/controller');
 
 const config = {
   name: urls.alternativeFormats,
   previous: urls.addContactDetails,
   next: urls.emailSubmit,
   validation: {
-    type: validation.checkboxes,
+    type: validation.radios,
     errors: {
       required:
         "Select which different format we should use when we contact you",
