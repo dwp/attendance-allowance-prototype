0192f93a5532995c3c6e1e08f48227d1f235d761
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2024-01-11 15:44:09 +0000

    chore: create sprint 78
    GitOrigin-RevId: eb6d6391e9e9896946416e34c8927342f3d9cb8e

---
+++ controllers/sprint-78/eligibility/birth-date.js
@@ -0,0 +1,43 @@
+const {
+  urls,
+  validation,
+  match,
+  registerController,
+} = require("../../../utils/controller");
+
+const config = {
+  name: urls.birthDate,
+  previous: [
+    {
+      page: urls.specialRulesDeclaration,
+    },
+  ],
+  next: [
+    {
+      page: urls.birthDateIneligible,
+      condition: {
+        field: "birth-date-year",
+        value: 1955,
+        match: match.greaterThan,
+      },
+    },
+    {
+      page: urls.otherBenefits,
+    },
+  ],
+  validation: {
+    type: validation.dateInput,
+    errors: {
+      required: 'Enter your date of birth',
+      requiredDay: 'Date of birth must include a day',
+      requiredMonth: 'Date of birth must include a month',
+      requiredYear: 'Date of birth must include a year',
+      validDay: 'Enter a real date of birth',
+      validMonth: 'Enter a real date of birth',
+      validYear: 'Enter a real date of birth',
+      invalidDate: 'Date of birth must be a real date',
+    },
+  },
+};
+
+module.exports = registerController(config.name, config);
