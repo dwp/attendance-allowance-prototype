475e4714baa74a78f41ed9e4f44b90ad0d0ac139
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2024-01-18 11:54:07 +0000

    feat: character count and limit

--- controllers/sprint-78/surgery-or-treatment/treatment-surgery-information.js
+++ controllers/sprint-78/surgery-or-treatment/treatment-surgery-information.js
@@ -24,8 +24,13 @@ const config = {
 
   validation: {
     type: validation.textInput,
+    options: {
+      minLength: 1,
+      maxLength: 200,
+    },
     errors: {
       required: 'Enter what treatment or surgery you are having and any dates',
+      maxLength: "Enter what treatment or surgery you are having and any dates in 200 characters or less",
     },
   },
 };
