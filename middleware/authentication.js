90e8f847aa7ccce08b8bc9ff9039134aa36a37a5
James Gordon <james.gordon@engineering.digital.dwp.gov.uk>
2024-02-26 10:35:15 +0000

    add second password option

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -7,7 +7,7 @@ const check = (name, pass, envVars) => {
 
   // Simple method to prevent short-circuit and use timing-safe compare
   valid = compare(name, envVars.USERNAME) && valid;
-  valid = compare(pass, envVars.PASSWORD) && valid;
+  valid = (compare(pass, envVars.PASSWORD) || compare(pass, envVars.INTERNAL_PASSWORD)) && valid;
 
   return valid;
 };
@@ -18,9 +18,10 @@ module.exports = (req, res, next) => {
   const useAuth = process.env.USE_AUTH;
   const username = process.env.USERNAME;
   const password = process.env.PASSWORD;
+  const internalPassword = process.env.INTERNAL_PASSWORD
 
   if (env === 'production' && useAuth === 'true') {
-    if (!username || !password) {
+    if (!username || (!password && !internalPassword)) {
       return res.send('<h1>Error:</h1><p>Username or password not set. <a href="https://govuk-prototype-kit.herokuapp.com/docs/publishing-on-heroku#6-set-a-username-and-password">See guidance for setting these</a>.</p>');
     }
 
