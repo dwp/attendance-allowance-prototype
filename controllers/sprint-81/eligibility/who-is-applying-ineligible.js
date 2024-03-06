c98e2b85545cf4bdaa74e7b804b91c5dcdd31680
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2024-03-06 13:15:05 +0000

    feat: updated back routing special rules

--- controllers/sprint-81/eligibility/who-is-applying-ineligible.js
+++ controllers/sprint-81/eligibility/who-is-applying-ineligible.js
@@ -1,32 +1,61 @@
 const {
-    urls,
-    match,
-    registerController,
-  } = require('../../../utils/controller');
-  
-  const config = {
-    name: urls.whoIsApplyingIneligible,
-    previous: [
-      {
-        page: urls.bestDescribe,
-        condition: {
-          field: urls.whoIsApplying,
-          value: 'someone-else',
-          match: match.value,
-        },
+  urls,
+  match,
+  registerController,
+} = require('../../../utils/controller');
+
+const config = {
+  name: urls.whoIsApplyingIneligible,
+  previous: [
+    {
+      page: urls.specialRulesDeclaration,
+      condition: {
+        field: urls.specialRulesDeclaration,
+        value: "yes",
+        match: match.value,
       },
-      {
-        page: urls.specialRulesDeclaration,
-        condition: {
-          field: urls.specialRulesDeclaration,
-          value: "yes",
-          match: match.value,
-        },
+    },
+    {
+      page: urls.bestDescribe,
+      condition: {
+        field: urls.bestDescribe,
+        value: 'powerOfAttorney',
+        match: match.value,
       },
-      {
-        page: urls.whoIsApplying,
+    },
+    {
+      page: urls.bestDescribe,
+      condition: {
+        field: urls.bestDescribe,
+        value: 'appointed',
+        match: match.value,
       },
-    ],
-  };
-  
-  module.exports = registerController(config.name, config);
+    },
+    {
+      page: urls.bestDescribe,
+      condition: {
+        field: urls.bestDescribe,
+        value: 'appointee',
+        match: match.value,
+      },
+    },
+    {
+      page: urls.bestDescribe,
+      condition: {
+        field: urls.bestDescribe,
+        value: 'cab',
+        match: match.value,
+      },
+    },
+    {
+      page: urls.bestDescribe,
+      condition: {
+        field: urls.bestDescribe,
+        value: 'curator',
+        match: match.value,
+      },
+    },
+  ],
+};
+
+module.exports = registerController(config.name, config);
