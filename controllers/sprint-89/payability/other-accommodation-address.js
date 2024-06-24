248655cfc0b848f58619a26156b932e60b0d5824
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2024-06-24 13:31:47 +0100

    feat: care home journey

---
+++ controllers/sprint-89/payability/other-accommodation-address.js
@@ -0,0 +1,40 @@
+const {
+  urls,
+  validation,
+  registerController,
+} = require("../../../utils/controller");
+
+const config = {
+  name: urls.careHomeAddress,
+  previous: urls.careHomeHospital,
+  next: urls.careHomeFunding,
+  validation: [
+    {
+      name: "care-home-name",
+      type: validation.textInput,
+      options: {
+        minLength: 1,
+        maxLength: 500,
+      },
+      errors: {
+        required: "Enter the name of the care or nursing home",
+        maxLength:
+          "Name of care or nursing home must be 500 characters or less",
+      },
+    },
+
+    {
+      name: "care-home-postcode",
+      type: validation.textInput,
+      options: {
+        regEx: /^([A-Za-z][A-Ha-hK-Yk-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/
+      },
+      errors: {
+        required: "Enter a postcode",
+        regEx: 'Enter a postcode in the correct format',
+      },
+    },
+  ],
+};
+
+module.exports = registerController(config.name, config);
