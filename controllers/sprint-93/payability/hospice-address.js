496ea7d4c78a7178294e5bc19471c49d492c51c8
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2024-08-09 11:59:33 +0100

    chore: create sprint 93

---
+++ controllers/sprint-93/payability/hospice-address.js
@@ -0,0 +1,40 @@
+const {
+  urls,
+  validation,
+  registerController,
+} = require("../../../utils/controller");
+
+const config = {
+  name: urls.hospiceAddress,
+  previous: urls.careHomeHospital,
+  next: urls.illnessDisability,
+  validation: [
+    {
+      name: "hospice-name",
+      type: validation.textInput,
+      options: {
+        minLength: 1,
+        maxLength: 150,
+      },
+      errors: {
+        required: "Enter the name of the hospice",
+        maxLength:
+          "Enter the name of the hospice in 150 characters or less",
+      },
+    },
+
+    {
+      name: "hospice-postcode",
+      type: validation.textInput,
+      options: {
+        regEx: /^([A-Za-z][A-Ha-hK-Yk-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/
+      },
+      errors: {
+        required: "Enter a postcode",
+        regEx: 'Enter a postcode in the correct format',
+      },
+    },
+  ],
+};
+
+module.exports = registerController(config.name, config);
