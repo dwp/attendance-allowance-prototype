fabcab293b21b5369bbcedbcdc61822d5bea89c8
jonathanbranthwaite <149800919+jonathanbranthwaite@users.noreply.github.com>
2024-10-01 12:24:42 +0100

    Merge pull request #158 from dwp/feat/locale-journey-type
    feat: configured new journeytype for special rules
    GitOrigin-RevId: 3359b21068dee61680650087573e7b35b922cfea

--- controllers/sprint-95/eligibility/best-describe.js
+++ controllers/sprint-95/eligibility/best-describe.js
@@ -12,7 +12,7 @@ const {
       {
         page: urls.helpingSomeoneIntro,
         condition: {
-          value: 'SREL',
+          value: 'SPECIAL_RULES',
           match: match.session,
         },
       },
