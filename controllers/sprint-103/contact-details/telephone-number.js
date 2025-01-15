0615f87bdb7d0e924a05a70a69a18abb2708609e
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2025-01-15 10:41:54 +0000

    feat: update appointee validation and check answers

--- controllers/sprint-103/contact-details/telephone-number.js
+++ controllers/sprint-103/contact-details/telephone-number.js
@@ -12,14 +12,14 @@ const config = {
     {
       name: "main-contact-number",
       type: validation.textInput,
-      options: {
+      /* options: {
         minLength: 5,
         maxLength: 15,
-      },
+      }, */
       errors: {
         required: 'Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 808 157 0192',
-        minLength: 'Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 808 157 0192',
-        maxLength: 'Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 808 157 0192',
+       /* minLength: 'Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 808 157 0192',
+        maxLength: 'Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 808 157 0192', */
       },
     },
   ],
