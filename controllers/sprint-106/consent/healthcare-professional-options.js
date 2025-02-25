47e851d19a10d6588abb1d5ef362833605035d33
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2025-02-25 09:35:15 +0000

    chore: create sprint 106

---
+++ controllers/sprint-106/consent/healthcare-professional-options.js
@@ -0,0 +1,40 @@
+const {
+  urls,
+  validation,
+  registerController,
+} = require("../../../utils/controller");
+
+const config = {
+  name: urls.healthcareProfessionalOptions,
+  previous: urls.healthcareProfessionalAdd,
+  next: urls.healthcareProfessionalDetails,
+  validation: [
+    {
+      name: "other-healthcare-professional-who",
+      type: validation.textInput,
+      options: {
+        minLength: 1,
+        maxLength: 50,
+      },
+      errors: {
+        required:
+          "Enter their profession or job title",
+        maxLength:
+          "Enter their profession or job title in 50 characters or less",
+      },
+      condition: {
+        field: "healthcare-professional-options",
+        value: "other-healthcare-professional",
+      },
+    },
+    {
+      name: "healthcare-professional-options",
+      type: validation.radios,
+      errors: {
+        required: "Select who you want to add contact details for",
+      },
+    },
+  ],
+}
+
+module.exports = registerController(config.name, config);
