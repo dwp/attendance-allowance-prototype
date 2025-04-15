5295ee2a4ab84d40529423cddb94774553d57e11
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2025-04-15 22:47:10 +0100

    chore: create evidence test 6

---
+++ controllers/evidence-test-6/identity/home-address-postcode.js
@@ -0,0 +1,36 @@
+const {
+  urls,
+  validation,
+  match,
+  registerController,
+} = require("../../../utils/controller");
+
+const config = {
+  name: urls.homeAddressPostcode,
+  previous: [
+    {
+      page: urls.residenceCountry,
+      condition: {
+        field: urls.residenceCountry,
+        value: 'scotland',
+        match: match.value,
+      },
+    },
+    {
+      page: urls.fullName,
+    },
+  ],
+  next: urls.homeAddressSelect,
+  validation: {
+    type: validation.textInput,
+    options: {
+      regEx: /^([A-Za-z][A-Ha-hK-Yk-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/
+    },
+    errors: {
+      required: "Enter a postcode",
+      regEx: 'Enter a postcode in the correct format',
+    },
+  },
+};
+
+module.exports = registerController(config.name, config);
